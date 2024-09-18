5d955f3c65bd1e59022cbb7e2239d62531afb090
Neil <morrison.neil.v@gmail.com>
2024-09-18 15:57:21 -0400

    updated styling etc.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,18 @@
 import { Typography } from "@/components";
+import classes from "./styles.module.css";
 
 export default function ProjectsHome() {
   return (
-    <div>
-      <Typography>Projects Overview</Typography>
+    <div className={classes["page-root"]}>
+      <div>
+        <Typography>Projects Overview</Typography>
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          optio rerum quod repudiandae modi, aliquid accusantium reprehenderit
+          sunt similique unde nobis maxime, ipsum ipsam. Assumenda dolore ea
+          expedita voluptatem quaerat.
+        </Typography>
+      </div>
     </div>
   );
 }
