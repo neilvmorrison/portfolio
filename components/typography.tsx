8bc5d149f2b58cc613421fab2d3f4ca095061c1e
Neil <morrison.neil.v@gmail.com>
2024-09-18 13:51:11 -0400

    updated

--- components/typography.tsx
+++ components/typography.tsx
@@ -1,10 +1,10 @@
 interface ITypographyProps {
-  component: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+  component?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
   children: string;
 }
 
 export default function Typography({
-  component: Component,
+  component: Component = "p",
   children,
 }: ITypographyProps) {
   return <Component>{children}</Component>;
